a4b05dc47b50ecc55697ee399cca087776a33bd9
abaykann <azizakbaral@gmail.com>
2025-02-05 20:11:05 +0700

    initial push

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("🚀 bismillah run http://{}", addr);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
